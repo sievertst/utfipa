de1c8608703d1966cdc53500ce2207a06067fa82
sievertst <sievert.stuelpnagel@mail.de>
2024-06-12 20:39:03 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "LaTeX package for typesetting Unicode-compliant IPA transcription";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+    let 
+      pkgs = nixpkgs.legacyPackages.${system};
+      pname = "utfipa";
+    in
+    {
+      packages.default = pkgs.stdenvNoCC.mkDerivation rec {
+        inherit pname;
+        version = "0.1";
+        outputs = [ "tex" ];
+        src = self;
+        nativeBuildInputs = [
+          (pkgs.writeShellScript "force-tex-output.sh" ''
+            out="''${tex-}"
+          '')
+        ];
+        installPhase = ''
+          runHook preInstall
+          path="$tex/tex/latex/${pname}"
+          mkdir -p "$path"
+          cp *.{sty,cls,def,clo} "$path"
+          runHook postInstall
+        '';
+      };
+    });
+}
